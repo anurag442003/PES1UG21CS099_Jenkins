3c3dfbdd0c6ede4d2f6d0475201af8d658b93e69
anurag442003 <anuragbhusare44@gmail.com>
2024-03-09 22:53:56 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "i have added a webhook\n";
+  cout << "i have added a webhooks\n";
   return 0;
 }
